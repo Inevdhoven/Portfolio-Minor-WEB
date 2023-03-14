18a0d693a4f7bc8d1f03753441785f7330312564
Inevdhoven <inevdhoven@gmail.com>
2023-03-14 13:28:05 +0100

    Added Vue files

---
+++ src/components/menu.vue
@@ -0,0 +1,13 @@
+
+<template>
+    <nav>
+        <a href="/">
+            <h1>InevandenHoven</h1>
+        </a>
+        <ul>
+            <li><a href="#about">About</a></li>
+            <li><a href="#my-work">My work</a></li>
+            <li><a href="mailto:inevdhoven@gmail.com">inevdhoven@gmail.com</a></li>
+        </ul>
+    </nav>
+</template>
