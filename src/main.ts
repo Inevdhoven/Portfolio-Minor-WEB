51d61bac3f77be386d7942d303fe8d83eeb5643b
Inevdhoven <inevdhoven@gmail.com>
2023-03-15 14:39:51 +0100

    Added my work and router

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
 import { createApp } from 'vue'
 import './style.css'
+import Header from './Header.vue'
 import App from './App.vue'
 import api from './modules/api.js'
 import router from './modules/router.js'
-import myWork from './modules/my-work.js'
+// import myWork from './modules/my-work.js'
 
-createApp(App).mount('#app')
+createApp(Header).mount('body > header')
+createApp(App).mount('body > main')
 
 
-api.dataDisplay().then((result) => {
-    // onRouteChanged(result);
-    console.log(result)
-}); 
-myWork.displayWork()
+api.displayData().then((result) => {
+    router.onRouteChanged(result);
+    // console.log(result)
+});
+
+api.displayMyWork().then((resultWork) => {
+    router.onRouteChanged(resultWork);
+    // console.log(resultWork)
+});
