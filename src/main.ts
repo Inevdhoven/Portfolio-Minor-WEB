272df25a10c077b3248049e455c0c3943214813d
Inevdhoven <inevdhoven@gmail.com>
2023-03-17 11:45:20 +0100

    Added event for menu button

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,14 @@ import { createApp } from 'vue'
 import './style.css'
 import Header from './Header.vue'
 import App from './App.vue'
-import api from './modules/api.js'
 import router from './modules/router.js'
-// import myWork from './modules/my-work.js'
 
 createApp(Header).use(router).mount('body > header')
 createApp(App).use(router).mount('body > main')
 
+const menuButton = document.querySelector('nav button');
+const menu = document.querySelector('nav ul');
 
-// api.displayData().then((result) => {
-//     router.onRouteChanged(result);
-//     // console.log(result)
-// });
-
-// api.displayMyWork().then((resultWork) => {
-//     router.onRouteChanged(resultWork);
-//     // console.log(resultWork)
-// });
+menuButton.addEventListener('click', () => {
+    menu.classList.toggle('menu-open');
+});
