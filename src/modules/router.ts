2ef52e81a8b776d8e10b7c57c07e1c84ee15d9c8
Inevdhoven <inevdhoven@gmail.com>
2023-03-17 11:47:49 +0100

    Added a vue router

--- src/modules/router.ts
+++ src/modules/router.ts
@@ -1,40 +1,3 @@
-// import { createApp } from 'vue'
-// import About from '../components/About.vue'
-// import MyWork from '../components/my-work.vue'
-
-// function onRouteChanged(data : any) {
-//     const hash = window.location.hash;
-//     const routerView = document.querySelector('body > main > section');
-//     console.log(hash)
-
-//     if (!(routerView instanceof HTMLElement)) {
-//         console.log(Error)
-//         throw new ReferenceError("No router view element available for rendering");
-//     }
-
-//     switch(hash) {
-//         case "":
-//             console.log("home")
-//             break;
-//         case "#about":
-//             createApp(About).mount('body > main > section')
-//             break;
-//         case "#my-work":
-//             createApp(MyWork).mount('body > main > section')
-//             break;
-//         default:
-//             routerView.innerHTML = `<h1>404</h1>`;
-//             break;
-//     } 
-// }
-
-// window.addEventListener("hashchange", onRouteChanged);
-
-// export default {
-//     onRouteChanged
-// }
-
-
 import {createRouter, createWebHistory} from 'vue-router'
 import About from '../components/About.vue'
 import MyWork from '../components/my-work.vue'
@@ -42,9 +5,9 @@ import Home from '../components/Home.vue'
 
 const routes = [
     {
-        path: '/', 
-        name: 'Home',
-        component: Home
+        path: '/', // default path
+        name: 'Home', // optional, but useful for debugging and readability
+        component: Home // the component to render
     },
     {
         path: '/about',
@@ -59,8 +22,8 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes
+    history: createWebHistory(import.meta.env.BASE_URL), // use HTML5 history API
+    routes 
 })
 
 export default router
